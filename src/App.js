587bbf7e0f68be49eb7a2dbc4080e5a97931d138
rmeza09 <rafasmez1009@gmail.com>
2025-04-23 23:05:27 -0400

    initial web app set up

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+// import Section from "./components/Section";
+// import Profile from "./components/Profile";
+// import Engineering from "./components/Engineering";
+// import Contact from "./components/Contact";
+// import Finance from "./components/Finance";
+// import PortfolioPie from "./components/PortfolioPie"
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./styles.css";
+
+import { Analytics } from "@vercel/analytics/react"
+
+function App() {
+  const [activeSection, setActiveSection] = useState("resume");
+
+  return (
+    <div>
+      <Navbar setActiveSection={setActiveSection} />
+
+      <div id="main-content">
+        <Section id="resume" isActive={activeSection === "resume"}>
+          <Profile />
+        </Section>
+
+        <Section id="engineering" isActive={activeSection === "engineering"}>
+          <Engineering />
+        </Section>
+        
+        <Section id="finance" title="Finance Research" isActive={activeSection === "finance"}>
+          <Finance/>
+          <PortfolioPie/>
+        </Section>
+        
+        <Section id="contact" isActive={activeSection === "contact"}>
+          <Contact />
+        </Section>
+      </div>
+      
+      <Analytics />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+        
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,  // Match image file extensions
+        type: "asset/resource", // Replaces file-loader in Webpack 5
+      },
+      {
+        test: /\.pdf$/,
+        type: "asset/resource", // Replaces file-loader in Webpack 5
+        generator: {
+          filename: "assets/[name][ext]" // Ensures PDFs are output to the assets folder
+        }
+      }
+      
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    static: "./dist",
+    port: 3000,
+  },
+};
